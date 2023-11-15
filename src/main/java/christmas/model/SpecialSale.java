1ebbe11f6355e10f72952020172b7014ea7f7b5e
skj4244 <skj4244@naver.com>
2023-11-15 18:38:27 +0900

    test: Special Sale

--- src/main/java/christmas/model/SpecialSale.java
+++ src/main/java/christmas/model/SpecialSale.java
@@ -4,14 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class SpecialSale {
-    private static final List<Integer> EVENT_DATE = List.of(3, 10, 17, 24, 25, 31);
+    static final List<Integer> EVENT_DATE = List.of(3, 10, 17, 24, 25, 31);
     private static final Integer MINIMUM_ORDER_PRICE = 10000;
+    static final Integer SPECIAL_SALE_PRICE = 1000;
 
     public static Integer getSpecialSalePrice(Integer wholePrice, LocalDate dateOfVisit) {
         if(!EVENT_DATE.contains(dateOfVisit.getDayOfMonth()) || wholePrice < MINIMUM_ORDER_PRICE){
             return 0;
         }
 
-        return -1000;
+        return -1*SPECIAL_SALE_PRICE;
     }
 }
